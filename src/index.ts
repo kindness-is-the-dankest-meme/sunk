1b6bff1e55a636318bfa09a79d7d245807d646c4
Matt Hayes <matt@mysterycommand.com>
2021-07-10 14:10:09 -0400

    feat: mouse position movement

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,26 @@ const k = kaboom({
   canvas,
   fullscreen: true,
 });
+
+k.scene("level-1", () => {
+  const [r, g, b] = [205, 133, 63].map((c) => c / 255);
+  if (!(r && g && b)) {
+    return;
+  }
+
+  const boat = k.add([
+    k.rect(40, 100),
+    k.pos(80, 80),
+    k.color(k.rgb(r, g, b)),
+    k.origin("center"),
+  ]);
+
+  k.mouseDown(() => {
+    const diff = k.mousePos().sub(boat.pos);
+    const dist = diff.len();
+
+    boat.move(diff.scale(Math.min(dist, 50 / dist)));
+  });
+});
+
+k.start("level-1");
