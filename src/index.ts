5fd533ec230cf1bf010b708df477912ec5c34a37
Matt Hayes <matt@mysterycommand.com>
2021-07-10 14:53:09 -0400

    docs: notes about where it came from

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,14 @@ const clamp = (x: number, lower: number, upper: number) =>
 
 const repeat = (t: number, m: number) => clamp(t - floor(t / m) * m, 0, m);
 
+/**
+ * @see https://github.com/mattdesl/lerp/blob/master/index.js
+ */
 const lerp = (v0: number, v1: number, t: number) => v0 * (1 - t) + v1 * t;
 
+/**
+ * @see https://gist.github.com/shaunlebron/8832585#gistcomment-3227412
+ */
 const lerpAngle = (a: number, b: number, t: number) => {
   const dt = repeat(b - a, ππ);
   return lerp(a, a + (dt > π ? dt - ππ : dt), t);
